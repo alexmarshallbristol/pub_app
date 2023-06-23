e3584afd7b342ab87bff0df3d0e3fb9ab6a120fd
Alex Marshall <alex.marshall@bristol.ac.uk>
2023-06-23 14:47:57 +0100

    garden points

--- open.py
+++ open.py
@@ -17,6 +17,7 @@ import moviepy.editor as mp
 import scipy
 from PIL import ImageFilter
 import time
+import datetime
 # speed by code - vectorise
 # make better output plots - height of sun cartoon etc
 # stitch two tif files together
@@ -38,7 +39,8 @@ cmap = LinearSegmentedColormap.from_list('sun_shade_cmap', colors)
 
 # data from https://environment.data.gov.uk/DefraDataDownload/?Mode=survey
 
-tif_file = "tif/DSM_ST5570_P_10631_20190117_20190117.tif"
+# tif_file = "tif/DSM_ST5570_P_10631_20190117_20190117.tif"
+tif_file = "tif/DSM_ST6070_P_10631_20190117_20190117.tif"
 # tif_file = "tif/homefarm.tif"
 # tif_file = "tif/perann.tif"
 tif_dataset = rasterio.open(tif_file)
@@ -65,7 +67,7 @@ def smooth_image(image_array, size=3):
     image = Image.fromarray(np.uint8(image_array*255))
     image = image.filter(ImageFilter.ModeFilter(size=size))
     image_array = np.array(image)
-    image_array = (image_array*_max)+_min
+    image_array = ((image_array*_max)+_min)/255
     return image_array
 
 
@@ -108,16 +110,24 @@ def smooth_image(image_array, size=3):
 
 # pub = [51.467334, -2.586485] # cadbury
 # pub = [51.468803, -2.593292] # cat and wheel
-pub = [51.453291, -2.609596] # hope and anchor
+# pub = [51.453291, -2.609596] # hope and anchor
 # pub = [51.455028, -2.590344] # castle park
 # pub = [51.220550, -0.341143] # home farm
 # pub = [51.220670, -0.334087] # tennis club
 # pub = [50.341347, -5.159108] # chris' gafff
 # pub = [51.458621, -2.601988] # uni
-pub_name = "University"
+pub = [51.45795467636116, -2.548439358008857] # seneca
+pub_name = "Seneca"
 
 output_gif = f"output_{pub_name.replace(' ','_')}"
 
+
+# from scipy.spatial import ConvexHull
+
+# def convexhull(p):
+# 	p = np.array(p)
+# 	hull = ConvexHull(p)
+# 	return p[hull.vertices,:]
 
 def compute_sun_direction(date, time, latitude, longitude):
     observer = ephem.Observer()
@@ -263,6 +273,14 @@ def _is_sunny(y_point, x_point, date, time, longitude, latitude, blur=True, N_su
 
     return sunny, azimuth
 
+def compute_distance(origin_x, origin_y, points_x, points_y):
+
+    distance_x = points_x-origin_x
+    distance_y = points_y-origin_y
+
+    distance = np.sqrt(distance_x**2 + distance_y**2)/upsampling
+
+    return distance
 
 
 
@@ -270,258 +288,173 @@ transformer_toOS = Transformer.from_crs("EPSG:4326", "EPSG:27700")
 idx = transformer_toOS.transform(pub[0],pub[1])
 idx = (idx[1], -idx[0])
 y_point_gps, x_point_gps = rasterio.transform.rowcol(transform, -idx[1], idx[0])
+print(y_point_gps, x_point_gps)
 y_point_gps*=upsampling
 x_point_gps*=upsampling
-print(y_point_gps, x_point_gps)
+print(y_point_gps, x_point_gps, 'upsampled')
+# boundaries = np.asarray([[51.45798598418041, -2.5484111551876887], [51.457932504048706, -2.548385674201808], [51.4579266546555, -2.5484647993684906],[51.457972196339945, -2.548487598145332]])
 
+boundaries = np.asarray([[51.45796107488286, -2.5484832507789275], [51.45792472509282, -2.5484691691814665], [51.45793141011384, -2.548386691253483],[51.45798530806013, -2.5484101605825846], [51.457976116166826, -2.5484557581362663],[51.45796441739083, -2.5484503937181864]])
+
+print(np.shape(boundaries))
+
+def no_floor(values):
+    return values
+boundaries_pixels = np.empty((0,2))
+for boundary_point in boundaries:
+    idx = transformer_toOS.transform(boundary_point[0],boundary_point[1])
+    idx = (idx[1], -idx[0])
+    y_point_gps_boundary, x_point_gps_boundary = rasterio.transform.rowcol(transform, -idx[1], idx[0], op=no_floor)
+    y_point_gps_boundary*=upsampling
+    x_point_gps_boundary*=upsampling
+    y_point_gps_boundary+= -y_point_gps
+    x_point_gps_boundary+= -x_point_gps
+    boundaries_pixels = np.append(boundaries_pixels, [[y_point_gps_boundary, x_point_gps_boundary]], axis=0)
+
+# plt.scatter(boundaries_pixels[:,0], boundaries_pixels[:,1])
+# plt.scatter(0, 0)
+# plt.show()
 
 date = '2023/06/22'  # Date format: yyyy/mm/dd
 # date = '2023/01/21'  # Date format: yyyy/mm/dd
 latitude = f'{pub[0]}'   # Latitude of the location
 longitude = f'{pub[1]}'  # Longitude of the location
 
-# time = '12:00:00'   # Time format: hh:mm:ss
-# time_iter = 0
-# for hour in range(10,21):
-#     for minute in ['00', 15, 30, 45]:
-#     # for minute in ['00', '05', 10, 15, 20, 25, 30, 35, 40, 45, 50, 55]:
-#         time_iter += 1
-#         time = f'{hour}:{minute}:00'   # Time format: hh:mm:ss
+steps = 15*upsampling # size of area to compute shadows
+buffer = int(steps/4.)
+boundaries_pixels = steps+buffer+boundaries_pixels
 
-for time_string in ["13:50:00"]:
+time_iter = 0
+for hour in range(6,21):
+    # for minute in ['00']:
+    for minute in ['00', 15, 30, 45]:
+    # for minute in ['00', '05', 10, 15, 20, 25, 30, 35, 40, 45, 50, 55]:
+        time_iter += 1
+        time_string = f'{hour}:{minute}:00'   # Time format: hh:mm:ss
+
+# for time_string in ["13:50:00"]:
         
         print(time_string)
 
-        # steps = 100 # size of area to compute shadows
-        steps = 25*upsampling # size of area to compute shadows
-        # steps = 3*upsampling # size of area to compute shadows
+
 
         L_shadow = 50
 
-        time_A = time.time()
-        is_sunny_array = np.zeros(np.shape(tif_data_full))
-        for y_point_idx, y_point_gps_i in enumerate(range(y_point_gps-steps, y_point_gps+steps+1)):
-            for x_point_idx, x_point_gps_i in enumerate(range(x_point_gps-steps, x_point_gps+steps+1)):
-                if y_point_idx == 0 and x_point_idx == 0:
-                    is_sunny, azimuth = _is_sunny(y_point_gps_i, x_point_gps_i, date, time_string, longitude, latitude, make_plots=True)
-                else:
-                    is_sunny, azimuth = _is_sunny(y_point_gps_i, x_point_gps_i, date, time_string, longitude, latitude)
-                is_sunny_array[y_point_gps_i][x_point_gps_i] = is_sunny
-        time_B = time.time()
-        print(f'{time_B-time_A:.3f}')
-
-        is_sunny_array = is_sunny_array[y_point_gps-L_shadow:y_point_gps+L_shadow,x_point_gps-L_shadow:x_point_gps+L_shadow]
-
-
-        time_A = time.time()
-
 
         yx_points = np.empty((0,4))
-        for y_point_idx, y_point_gps_i in enumerate(range(y_point_gps-steps, y_point_gps+steps)):
-            for x_point_idx, x_point_gps_i in enumerate(range(x_point_gps-steps, x_point_gps+steps)):
+        for y_point_idx, y_point_gps_i in enumerate(range(y_point_gps-steps, y_point_gps+steps+1)):
+            for x_point_idx, x_point_gps_i in enumerate(range(x_point_gps-steps, x_point_gps+steps+1)):
                 yx_points = np.append(yx_points, [[y_point_gps_i, x_point_gps_i, y_point_idx, x_point_idx]], axis=0)
         
         yx_points = yx_points.astype(int)
-        print(np.shape(yx_points))
 
-        rows = np.arange(steps*2+1).reshape(steps*2+1, 1)-steps
-        cols = np.arange(steps*2+1).reshape(1, steps*2+1)-steps
-        meshgrid_rows, meshgrid_cols = np.meshgrid(rows, cols)
-        result_array = np.stack((meshgrid_cols,meshgrid_rows), axis=2)
-        print(np.shape(result_array))
-        result_array = result_array.reshape((np.shape(result_array)[0]*np.shape(result_array)[1],np.shape(result_array)[2]))
-        print(np.shape(result_array))
-        result_array_gps = result_array.copy()
-        result_array_gps[:,0] += y_point_gps
-        result_array_gps[:,1] += x_point_gps
-        result_array_overall = np.concatenate((result_array_gps, result_array),axis=1)
-
-
-        # for i in yx_points:
-            # print(i)
-        # quit()
-
-        
-        
         tif_data_samples = np.asarray([tif_data_full[coords[0]-L_shadow:coords[0]+L_shadow,coords[1]-L_shadow:coords[1]+L_shadow] for coords in yx_points])
-
-        print(np.shape(tif_data_samples))
-        plt.imshow(tif_data_samples[0])
-        plt.savefig('A2')
-        plt.close('all')
 
         rows = np.arange(L_shadow*2).reshape(L_shadow*2, 1)
         cols = np.arange(L_shadow*2).reshape(1, L_shadow*2)
         meshgrid_rows, meshgrid_cols = np.meshgrid(rows, cols)
         result_array = np.stack((meshgrid_cols,meshgrid_rows), axis=2)
         angles = compute_angle_from_north(L_shadow, L_shadow, result_array[:,:,0], result_array[:,:,1])
-        plt.imshow(angles)
-        plt.savefig('B2')
-        plt.close('all')
+
         azimuth, altitude = compute_sun_direction(date, time_string, latitude, longitude)
-        # where = select_elements_within_angle(angles, azimuth, 10, L_shadow)
         where = select_elements_without_angle(angles, azimuth, 10, L_shadow)
 
-        def compute_distance(origin_x, origin_y, points_x, points_y):
-
-            distance_x = points_x-origin_x
-            distance_y = points_y-origin_y
-
-            distance = np.sqrt(distance_x**2 + distance_y**2)/upsampling
-
-            return distance
-
-        heights = tif_data_full[y_point_gps-L_shadow:y_point_gps+L_shadow,x_point_gps-L_shadow:x_point_gps+L_shadow]
-
-        # plt.imshow(heights)
-        # plt.savefig('H2')
-        # plt.close('all')
-
         distance_to_centre = compute_distance(L_shadow, L_shadow, result_array[:,:,0], result_array[:,:,1])
-        # plt.imshow(distance_to_centre)
-        # plt.savefig('Dist2')
-        # plt.close('all')
-
-        # HERE ALL GOOD
-
-        # distance_to_centre = distance_to_centre[where]
-        # tif_data_samples = tif_data_samples[:,where[1],where[0]]
-        # tif_data_samples[:,where] = 0
         tif_data_samples[:,where[0],where[1]] = 0
 
-        # plt.imshow(tif_data_samples[0])
-        # plt.savefig('E2')
-        # plt.close('all')
-        # quit()
+        tif_data_samples_diff = tif_data_samples-np.expand_dims(np.expand_dims(tif_data_samples[:,L_shadow,L_shadow],1),1)
 
-
-        # height_at_this_point = heights[result_array_overall[:,2], result_array_overall[:,3]]
-        height_at_this_point = heights.reshape(((steps*2)**2))
-        # print(np.shape(heights))
-        # print(np.shape(result_array_overall))
-        # print(np.shape(height_at_this_point))
-        # print(np.shape(tif_data_samples))
-        # quit()
-
-        # tif_data_samples_diff = tif_data_samples-np.expand_dims(np.expand_dims(height_at_this_point,1),1)
-        tif_data_samples_diff = tif_data_samples-np.expand_dims(np.expand_dims(tif_data_samples[:,steps,steps],1),1)
-
-        print(np.shape(tif_data_samples_diff), np.shape(distance_to_centre))
         angle = np.degrees(np.arctan2(tif_data_samples_diff,np.expand_dims(distance_to_centre,0)))
 
-        print(np.shape(angle))
         angle_max = np.amax(angle, axis=(1,2))
-        print(np.shape(angle_max))
 
         where = np.where(angle_max>altitude) # where in dark
         is_sunny = np.ones(np.shape(angle_max))
         is_sunny[where] = 0.
-        is_sunny = is_sunny.reshape((steps*2,steps*2))
-        print(np.shape(is_sunny))
+        is_sunny = is_sunny.reshape((steps*2+1,steps*2+1))
 
-        plt.subplot(1,2,1)
-        plt.imshow(is_sunny_array)
-        plt.subplot(1,2,2)
-        plt.imshow(is_sunny)
-        plt.savefig("output.png")
-        plt.show()
+        is_sunny = smooth_image(is_sunny, size=4)
 
-        print(np.sum(is_sunny_array))
-        print(np.sum(is_sunny))
-
-        quit()
+        # tif_data_i = tif_data_full[y_point_gps-steps:y_point_gps+steps+1,x_point_gps-steps:x_point_gps+steps+1]
+        # plt.imshow(tif_data_i, norm=LogNorm())
+        # plt.imshow(is_sunny, cmap=cmap, alpha=0.5)        
+        # plt.tight_layout()        
+        # plt.show()
 
 
-        # for coords in yx_points:
-        #     array_i = tif_data_full[coords[0]-L_shadow:coords[0]+L_shadow,coords[1]-L_shadow:coords[1]+L_shadow]
-        #     # print(np.shape(array_i))
-        #     array_i[where] = 0.
-        #     # print(np.shape(array_i))
-        #     plt.imshow(array_i)
-        #     plt.show()
-        #     quit()
+
+
+        is_sunny_pad = np.pad(is_sunny, buffer)
+        tif_data_i = tif_data_plain_upsampling[y_point_gps-steps-buffer:y_point_gps+steps+1+buffer,x_point_gps-steps-buffer:x_point_gps+steps+1+buffer]
+
+        plt.figure(figsize=(12,4))
+
+        ax = plt.subplot(1,3,1)
+        plt.imshow(tif_data_i, norm=LogNorm())
+        # plt.fill(boundaries_pixels[:,1], boundaries_pixels[:,0], c='w',alpha=0.5)
+        boundaries_pixels_swap = boundaries_pixels.copy()
+        boundaries_pixels_swap[:,1] = boundaries_pixels[:,0]
+        boundaries_pixels_swap[:,0] = boundaries_pixels[:,1]
+        poly = plt.Polygon(boundaries_pixels_swap, ec="w", fill=False)
+        path = poly.get_path()
+
+
+        rows = np.arange(steps*2+buffer*2+1).reshape(steps*2+buffer*2+1, 1)
+        cols = np.arange(steps*2+buffer*2+1).reshape(1, steps*2+buffer*2+1)
+        meshgrid_rows, meshgrid_cols = np.meshgrid(rows, cols)
+        result_array = np.stack((meshgrid_cols,meshgrid_rows), axis=2)
+        result_array = result_array.reshape(((steps*2+buffer*2+1)**2,2))
+        result_array_swap = result_array.copy()
+        result_array_swap[:,1] = result_array[:,0]
+        result_array_swap[:,0] = result_array[:,1]
+        where_contained = path.contains_points(result_array_swap)
+        # plt.scatter(result_array[where_contained][:,0], result_array[where_contained][:,1])
+
+        is_sunny_pad_garden = np.expand_dims(is_sunny_pad, -1)
+        is_sunny_pad_garden = is_sunny_pad_garden.reshape(((steps*2+buffer*2+1)**2,1))
+        is_sunny_pad_garden = np.concatenate((is_sunny_pad_garden,result_array_swap),-1)
+        # is_sunny_pad_garden = is_sunny_pad_garden[where_contained]
 
         # quit()
+        # plt.scatter()
 
-        tif_data_samples = np.asarray([[coords[2], coords[3], np.amax(tif_data_full[coords[0]-L_shadow:coords[0]+L_shadow,coords[1]-L_shadow:coords[1]+L_shadow][where])] for coords in yx_points])
-        # print(np.shape(tif_data_samples))
+        ax.add_patch(poly)
+        rect = patches.Rectangle((buffer-0.5, buffer-0.5), steps*2+1, steps*2+1, linewidth=2, edgecolor='red', facecolor='none')
+        ax.add_patch(rect) 
+        plt.text(0.01, 0.99, f'Time: {time_string}', horizontalalignment='left',verticalalignment='top', transform=ax.transAxes, c='w')
+        plt.text(0.01, 0.94, f'Date: {date}', horizontalalignment='left',verticalalignment='top', transform=ax.transAxes, c='w')
+        plt.yticks([],[])   
+        plt.xticks([],[]) 
+        # plt.colorbar()
 
-        
-
-        where = np.where(tif_data_samples[:,2]>altitude)
-        tif_data_samples[:,2] = np.ones(np.shape(tif_data_samples[:,2]))
-        tif_data_samples[where] = 0
-
-
-        # print(tif_data_samples[0])
-        square_array = np.zeros((np.max(tif_data_samples[:, 0].astype(int))+1,np.max(tif_data_samples[:, 0].astype(int))+1))
-        print(np.shape(square_array))
-        square_array[tif_data_samples[:, 0].astype(int), tif_data_samples[:, 2].astype(int)] = tif_data_samples[:,2]
-
-        print(np.shape(square_array))
-
-        plt.imshow(square_array)
-        plt.show()
-
-        # print(where)
-        quit()
-        angles = np.expand_dims(angles, 0)
-        angles = np.repeat(angles, np.shape(tif_data_samples)[0], axis=0)
-
-        print(np.shape(tif_data_samples), np.shape(angles))
-
-
-
-
-
-
-        quit()
-
-        is_sunny, azimuth = _is_sunny_vec(y_point_i, x_point_i, date, time_string, longitude, latitude)
-        is_sunny_array[y_point_i][x_point_i] = is_sunny
-        time_B = time.time()
-        print(f'{time_B-time_A:.3f}')
-        quit()
-
-
-
-        N_pixels = 30*upsampling # number of metres around point to plot
-        # tif_data_i = tif_data_full[y_point_gps-N_pixels:y_point_gps+N_pixels,x_point_gps-N_pixels:x_point_gps+N_pixels]
-        tif_data_i = tif_data_plain_upsampling[y_point_gps-N_pixels:y_point_gps+N_pixels,x_point_gps-N_pixels:x_point_gps+N_pixels]
-        is_sunny_array_i = is_sunny_array[y_point_gps-N_pixels:y_point_gps+N_pixels,x_point_gps-N_pixels:x_point_gps+N_pixels]
-
-        # is_sunny_array_i = gaussian_filter(is_sunny_array_i, sigma=0.5)
-        is_sunny_array_i = smooth_image(is_sunny_array_i, size=4)
-
-        
-        is_sunny, azimuth = _is_sunny(y_point_gps, x_point_gps, date, time_string, longitude, latitude, make_plots=True)
-
-        fig = plt.figure(figsize=(10, 6))
-        fig.suptitle(f"{pub_name} {time_string} {date}", fontsize=14)
-
-        ax = plt.subplot(1,2,1)
+        ax = plt.subplot(1,3,2)
         plt.imshow(tif_data_i, norm=LogNorm())
-        rect = patches.Rectangle((N_pixels-steps-0.5, N_pixels-steps-0.5), steps*2+1, steps*2+1, linewidth=2, edgecolor='red', facecolor='none')
-        ax.add_patch(rect)
-        angle_radians = np.radians(azimuth)
-        line_length = N_pixels*4
-        x_endpoint = -line_length * np.cos(angle_radians)
-        y_endpoint = line_length * np.sin(angle_radians)
-        ax.plot([N_pixels-0.5, N_pixels-0.5 + y_endpoint], [N_pixels-0.5, N_pixels-0.5 + x_endpoint], color='red')
-        plt.xlim(0-0.5, N_pixels*2-0.5)
-        plt.ylim(N_pixels*2-0.5,-0.5)
+        plt.imshow(is_sunny_pad, cmap=cmap, alpha=0.5, vmin=0, vmax=1)    
+        # plt.colorbar()
+        rect = patches.Rectangle((buffer-0.5, buffer-0.5), steps*2+1, steps*2+1, linewidth=2, edgecolor='red', facecolor='none')
+        ax.add_patch(rect) 
+        plt.fill(boundaries_pixels[:,1], boundaries_pixels[:,0], c='w',alpha=0.25)
+        plt.yticks([],[])   
+        plt.xticks([],[])   
 
-        ax = plt.subplot(1,2,2)
-        plt.imshow(tif_data_i, norm=LogNorm())
-        plt.imshow(is_sunny_array_i, cmap=cmap, alpha=0.5)
-        rect = patches.Rectangle((N_pixels-steps-0.5, N_pixels-steps-0.5), steps*2+1, steps*2+1, linewidth=2, edgecolor='red', facecolor='none')
-        ax.add_patch(rect)
 
+
+        is_sunny_pad_garden = is_sunny_pad_garden[where_contained]
+
+        ax = plt.subplot(1,3,3)
+        plt.scatter(is_sunny_pad_garden[:,1], is_sunny_pad_garden[:,2], c=is_sunny_pad_garden[:,0], vmin=0, vmax=1)
+        plt.gca().invert_yaxis()
+        plt.yticks([],[])   
+        plt.xticks([],[])   
+
+
+        plt.subplots_adjust(hspace=0,wspace=0)
         plt.tight_layout()        
-        # plt.savefig(f'temp/plot_{time_iter}.png')
-        plt.savefig(f'test_mode4x2.png')
+        plt.savefig(f'temp/plot_{time_iter}.png')
+        # plt.show()
         plt.close('all')
-        quit()
+
+
 
 image_files = glob.glob('temp/plot_*.png')
 image_idx_max = 0
@@ -543,7 +476,7 @@ for image_file in image_files:
     frames.append(image)
 
 # Save frames as an animated GIF
-frames[0].save(output_gif+".gif", format='GIF', append_images=frames[1:], save_all=True, duration=200, loop=0)
+frames[0].save(output_gif+".gif", format='GIF', append_images=frames[1:], save_all=True, duration=200*2, loop=0)
 clip = mp.VideoFileClip(output_gif+".gif")
 clip.write_videofile(f"mp4/{output_gif}.mp4")
 
